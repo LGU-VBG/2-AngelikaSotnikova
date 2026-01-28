acdcf1a65e33f0bbfbbb2fdff2a97409d783c9f8
AngelikaSotnikova <98815627+AngelikaSotnikova@users.noreply.github.com>
2026-01-28 20:28:29 +0300

    Add files via upload

---
+++ Bake.cs
@@ -0,0 +1,26 @@
+﻿namespace Baker
+{
+    class Bake
+    {
+        public static int temperature = 25; 
+        public static Random num = new Random(); 
+        public delegate void BakeDekegate(); 
+        public static event BakeDekegate WetStart = null; 
+        public static event BakeDekegate WetMessage = null; 
+
+        public static void Baking() 
+        {
+            Console.WriteLine("Начат процесс нагревания печи."); 
+
+            for (int t = temperature; t < 220; t = temperature) 
+            {
+                Console.WriteLine($"Температура {t} °C, нагревание...");
+                int temper = num.Next(15, 25); 
+                temperature += temper; 
+            }
+            
+            WetMessage(); 
+            WetStart(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Baker
+{
+    class Program
+    {
+        delegate void Lambda();
+        static void Main(string[] args)
+        {
+            Bake.WetMessage += BakeIsReady;
+            Bake.WetStart += WetLevel;
+
+            Bake.Baking(); 
+        }
+
+        static void WetLevel() 
+        {
+            int wet = 60; 
+            Random num = new Random();
+
+            for (int mins = 0; mins < 12; mins++) 
+            {
+                if (wet > 20)
+                {
+                    int wt = num.Next(5, 10);
+                    wet -= wt;
+
+                    Console.WriteLine($"Идет процесс выпечки, текущая влажность {wet}%. ");
+                }
+                else
+                {
+                    Console.WriteLine("Низкая влажность, подача пара...");
+                    wet = 50;
+                }
+            }
+        }
+        static void BakeIsReady() 
+        {
+            Lambda stroka = () => Console.WriteLine("Температура достигнута, печь готова к работе, влажность 60%.");
+            stroka();
+        }
+    }
+}
