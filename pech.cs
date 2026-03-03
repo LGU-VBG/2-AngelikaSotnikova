ee98faee4c38fde490d55d03334a19eaed990a66
AngelikaSotnikova <98815627+AngelikaSotnikova@users.noreply.github.com>
2026-03-03 08:19:36 +0300

    Add files via upload

---
+++ pech.cs
@@ -0,0 +1,31 @@
+﻿namespace Hleb
+{
+    public static class Pech
+    {
+        public static int temp { get; set; } = 10;
+        public static int vlazh { get; set; } = 60;
+
+        public static void Gotovka()
+        {
+            Random rnd = new Random();
+            while (temp < 220)
+            {
+                int randVlazh = rnd.Next(5, 10);
+                int randTemp = rnd.Next(10, 25);
+                Console.WriteLine($"Прошло 10 минут. Температура повышается на {randTemp} градусов.");
+                temp += randTemp;
+                Console.WriteLine($"Всего {temp} градусов.");
+                vlazh -= randVlazh;
+                Console.WriteLine($"Уровень влажности {vlazh}.");
+                if (vlazh <= 20)
+                {
+                    Pekar.DobPar(); 
+                }
+            }
+            if (temp >= 220)
+            {
+                Pekar.HlebInPech(); 
+            }
+        }
+    }
+}
---
+++ pekar.cs
@@ -0,0 +1,15 @@
+﻿namespace Hleb
+{
+    public static class Pekar
+    {
+        public static void DobPar()
+        {
+            Pech.vlazh += 50;
+            Console.WriteLine($"Пекарь повысил влажность. Теперь в печи влажность {Pech.vlazh} %");
+        }
+        public static void HlebInPech()
+        {
+            Console.WriteLine("Пекарь загружает хлеб");
+        }
+    }
+}
